dc34083f7f22f4a3702f83204cbd45292affded5
bvsd1042001 <150139717+bvsd1042001@users.noreply.github.com>
2024-06-20 14:03:02 +0530

    Add files via upload

---
+++ TeamMemberController.cs
@@ -0,0 +1,24 @@
+﻿using ResponsiveTeamMemberWebPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResponsiveTeamMemberWebPage.Controllers
+{
+    public class TeamMemberController : Controller
+    {
+        private List<TeamMember> _teamMembers = new List<TeamMember>
+    {
+        new TeamMember { Id = 1, Name = "John Doe", Position = "CEO", ImageUrl = "/images/john-doe.jpg" },
+        new TeamMember { Id = 2, Name = "Jane Doe", Position = "CTO", ImageUrl = "/images/jane-doe.jpg" },
+        // Add more team members as needed
+    };
+        public ActionResult Index()
+        {
+            return View(_teamMembers);
+        }
+
+    }
+}
